7015cfc681c18a979ca5fc1adb416b21133c5007
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2019-09-13 14:50:09 -0400

    Update GibsoniaCraft.java

--- plugin/src/lightling/gibsoniacraft/GibsoniaCraft.java
+++ plugin/src/lightling/gibsoniacraft/GibsoniaCraft.java
@@ -3,16 +3,20 @@ package lightling.gibsoniacraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import lightling.gibsoniacraft.crafting.Excavator;
+import lightling.gibsoniacraft.util.BlockListener;
 import lightling.gibsoniacraft.util.PlayerInteractListener;
 
 public final class GibsoniaCraft extends JavaPlugin {
 	
 	private Excavator excavatorClass;
 	private PlayerInteractListener pListener;
+	private BlockListener bListener;
 	
 	@Override
 	public void onEnable() {
 		excavatorClass = new Excavator(this);
+		pListener = new PlayerInteractListener(this);
+		bListener = new BlockListener(this);
 	}
 	
 	@Override
